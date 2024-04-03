5d4e045b564a6ac684b2e4c31a8d813563b48918
chienhuak <christy.kch.c@gmail.com>
2024-04-03 13:48:36 +0800

    After positioned by row 14, search entire column, replace text

--- UsercommentU.js
+++ UsercommentU.js
@@ -12,32 +12,54 @@ function replaceTextInRow14ForMultipleFiles() {
       // 添加更多文件的 URL
     ];
   
+  
     // 遍历每个文件的 URL
     fileUrls.forEach(function(fileUrl) {
-      // 根据文件的 URL 打开电子表格
-      var spreadsheet = SpreadsheetApp.openByUrl(fileUrl);
-      // 获取所有的工作表
-      var sheets = spreadsheet.getSheets();
-      
-      // 遍历每个工作表
-      sheets.forEach(function(sheet) {
-        // 获取工作表的数据范围
-        var range = sheet.getRange("14:14");
-        // 获取数据范围内的所有单元格的值
-        var values = range.getValues()[0]; // 只获取第一行的值，因为我们只搜索第 14 行
+      try {
+        // 根据文件的 URL 打开电子表格
+        var spreadsheet = SpreadsheetApp.openByUrl(fileUrl);
+        // 获取所有的工作表
+        var sheets = spreadsheet.getSheets();
         
-        // 遍历第 14 行的每个单元格
-        values.forEach(function(cellValue, col) {
-          // 检查单元格的值是否包含特定文本
-          if (typeof cellValue === 'string' && cellValue.indexOf("User CommentU") !== -1) {
-            // 将包含特定文本的整个单元格内容替换为新文本
-            values[col] = "User Comment";
+        // 遍历每个工作表
+        sheets.forEach(function(sheet) {
+          Logger.log("正在处理工作表：" + sheet.getName());
+          
+          // 获取第 14 行的数据范围
+          var row14Range = sheet.getRange(14, 1, 1, sheet.getLastColumn());
+          // 获取第 14 行的所有单元格的值
+          var row14Values = row14Range.getValues()[0];
+          Logger.log("第 14 行的值：" + row14Values.join(", "));
+          
+          // 手动指定 "User Comment" 的单元格列索引
+          var columnIndex = row14Values.indexOf("User Comment") !== -1 ? row14Values.indexOf("User Comment") + 1 : 0;
+          Logger.log("找到 'User Comment' 的单元格在第 " + columnIndex + " 列");
+          
+          // 如果找到 "User Comment" 的单元格
+          if (columnIndex > 0) {
+            // 获取该列的数据范围，从第 15 行到最后一行
+            var columnRange = sheet.getRange(15, columnIndex, sheet.getLastRow() - 14, 1);
+            // 获取该列的所有单元格的值
+            var columnValues = columnRange.getValues();
+            
+            // 遍历该列的每个单元格
+            columnValues.forEach(function(cellRow, rowIndex) {
+              // 检查单元格的值是否包含特定文本
+              if (typeof cellRow[0] === 'string' && cellRow[0].indexOf("User CommentU") !== -1) {
+                Logger.log("找到 'User CommentU' 的单元格在第 " + (rowIndex + 15) + " 行，第 " + columnIndex + " 列");
+                // 将包含特定文本的单元格内容替换为 "User Comment"
+                cellRow[0] = "User Comment";
+              }
+            });
+            
+            // 更新工作表中指定范围的单元格内容
+            columnRange.setValues(columnValues);
+            Logger.log("更新了工作表：" + sheet.getName());
           }
         });
-        
-        // 更新工作表中第 14 行的单元格内容
-        range.setValues([values]);
-      });
+      } catch (error) {
+        // 捕获并记录错误
+        Logger.log("在处理 " + fileUrl + " 时出现错误：" + error);
+      }
     });
   }
-  
